b4e62af4bd4d54adf86278fd1580fe43a3095d96
Vladyslav Shatulskyi <vladyslav.shatulskyi@flaggerforce.com>
2025-08-20 18:35:12 +0300

    Add multiple browsers

--- Common.Playwright/Browser/PlaywrightBrowserFactory.cs
+++ Common.Playwright/Browser/PlaywrightBrowserFactory.cs
@@ -13,10 +13,20 @@ public class PlaywrightBrowserFactory
 
     public async Task<IBrowser> Create(IPlaywright playwright)
     {
-        return await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        var browserType = Environment.GetEnvironmentVariable("PlaywrightBrowserName") ?? "chromium";
+
+        var launchOptions = new BrowserTypeLaunchOptions
         {
             Headless = _config.Headless,
             SlowMo = _config.SlowMo
-        });
+        };
+
+        return browserType.ToLowerInvariant() switch
+        {
+            "chromium" => await playwright.Chromium.LaunchAsync(launchOptions),
+            "firefox" => await playwright.Firefox.LaunchAsync(launchOptions),
+            "webkit" => await playwright.Webkit.LaunchAsync(launchOptions),
+            _ => throw new ArgumentException($"Unsupported browser type: {browserType}")
+        };
     }
 }
